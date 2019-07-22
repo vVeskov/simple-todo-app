0f4c4bbb335a461698b93608a121d44b8602e413
unknown <vv_softuni@abv.bg>
2019-07-22 19:18:45 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,155 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <button @click="changeTab('AddToDo')">Add</button>
+    <button @click="changeTab('IncompletedToDos')">ToDo</button>
+    <button @click="changeTab('CompletedTodo')">Complete</button>
+    <keep-alive>
+      <component :is="activeTab"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable*/
+import AddToDo from "./components/AddToDo";
+import CompletedTodo from "./components/CompletedToDos";
+import IncompletedToDos from "./components/IncompletedToDos";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      activeTab: ""
+    };
+  },
   components: {
-    HelloWorld
+    AddToDo,
+    IncompletedToDos,
+    CompletedTodo
+  },
+  methods: {
+    changeTab(tabName) {
+      this.activeTab = tabName;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Global Style */
+body {
+  background: #fff;
+  color: #333;
+  font-family: Lato, sans-serif;
+}
+.container {
+  display: block;
+  width: 400px;
+  margin: 100px auto 0;
+}
+ul {
+  margin: 0;
+  padding: 0;
+}
+li * {
+  float: left;
+}
+li,
+h3 {
+  clear: both;
+  list-style: none;
+}
+input,
+button {
+  outline: none;
+}
+button {
+  background: none;
+  border: 0px;
+  color: #888;
+  font-size: 15px;
+  width: 60px;
+  margin: 10px 0 0;
+  font-family: Lato, sans-serif;
+  cursor: pointer;
+}
+button:hover {
+  color: #333;
+}
+h3,
+label[for="new-task"] {
+  color: #333;
+  font-weight: 700;
+  font-size: 15px;
+  border-bottom: 2px solid #333;
+  padding: 30px 0 10px;
+  margin: 0;
+  text-transform: uppercase;
+}
+input[type="text"] {
+  margin: 0;
+  font-size: 18px;
+  line-height: 18px;
+  height: 18px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  background: #fff;
+  border-radius: 6px;
+  font-family: Lato, sans-serif;
+  color: #888;
+}
+input[type="text"]:focus {
+  color: #333;
+}
+
+/* Add Item  */
+label[for="new-task"] {
+  display: block;
+  margin: 0 0 20px;
+}
+input#new-task {
+  float: left;
+  width: 318px;
+}
+p > button:hover {
+  color: #0fc57c;
+}
+/* ToDo List */
+li {
+  overflow: hidden;
+  padding: 20px 0;
+  border-bottom: 1px solid #eee;
+}
+li > label {
+  font-size: 18px;
+  line-height: 40px;
+  width: 237px;
+  padding: 0 0 0 11px;
+}
+
+button {
+  margin: 0 0 0 10px;
+}
+/* Completed Tasks */
+#completed-tasks label {
+  text-decoration: line-through;
+  color: #888;
+}
+
+li {
+  overflow: hidden;
+  padding: 20px 0;
+  border-bottom: 1px solid #eee;
+}
+li > label {
+  font-size: 18px;
+  line-height: 40px;
+  width: 237px;
+  padding: 0 0 0 11px;
+}
+li > .delete:hover {
+  color: #cf2323;
 }
 </style>
+
+
---
+++ src/components/AddToDo.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>
+    <label for="new-task">Add Item</label>
+    <input id="new-task" type="text" />
+    <button>Add</button>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "add-todo"
+};
+</script>
+
+<style>
+</style>
